98b0bbe7f1631241169d531d5642260af4162a7c
Seth Parson <defectiveseth@gmail.com>
2014-11-13 03:21:40 -0500

    Tag 6.0.3.11

--- BestSellerCommandHandler.lua
+++ BestSellerCommandHandler.lua
@@ -3,120 +3,29 @@ function bsCommandHandler(self,msg)
   narg,numarg=sml_nargify(msg)
   sml_dprint(db.Debug,"BestSeller","msg=["..msg.."]")
   sml_dprint(db.Debug,"BestSeller","Number of args: "..tostring(narg))
-  if(narg[0] == "testmoney" ) then
-	sml_print("BestSeller",format("I have %d gold %d silver %d copper.", bsGetGold(), bsGetSilver(), bsGetCopper() ) )
-  end
-  if(narg[0] == nil) or
-	(narg[0] == "show" ) then
-	BSAMFrame:Show()
-  end
-  if(narg[0] == "hide" ) then
-	BSAMFrame:Hide()
-  end
-  if(narg[0] == "minishow" ) then
-	sml_print("BestSeller","Showing mini map button.")
-	BSMMButton:Show()
-  end
-  if(narg[0] == "minihide" ) then
-
-	sml_print("BestSeller","Hiding mini map button.")
-	BSMMButton:Hide()
-  end
-  if(narg[0] == "minireset" ) then
-	db.MMX=-80
-	db.MMY=-160
-	BSMMButton:SetPoint("TOPLEFT", db.MMX, db.MMY)
-  end
+  if(narg[0] == "testmoney" ) then sml_print("BestSeller",format("I have %d gold %d silver %d copper.", bsGetGold(), bsGetSilver(), bsGetCopper() ) ) end
+  if(narg[0] == nil) or (narg[0] == "show" ) then BSAMFrame:Show() end
+  if(narg[0] == "hide" ) then BSAMFrame:Hide() end
+  if(narg[0] == "minishow" ) then sml_print("BestSeller","Showing mini map button.") BSMMButton:Show()  end
+  if(narg[0] == "minihide" ) then sml_print("BestSeller","Hiding mini map button.") BSMMButton:Hide() end
+  if(narg[0] == "minireset" ) then db.MMX=-80 db.MMY=-160 BSMMButton:SetPoint("TOPLEFT", db.MMX, db.MMY) end
   if(narg[0] == "help" )
-	or ( msg == "?" ) then
-	bsShowHelp()
-	return
-  end
-  if (narg[0] == "hoverframeinfo") or (msg=="hfi") then
-	local curframe=GetMouseFocus():GetName()
-	sml_print("BestSeller",curframe)
-  end
-  if(narg[0] == "who") then
-	SendWho('80')
-	SortWho('Guild')
-	numWhos, totalCount = GetNumWhoResults()
-	sml_dprint(db.Debug,"BestSeller",numWhos.." "..totalCount)
-  end
-  if(narg[0] == "ssz") then
-	local az=MinimapZoneTextButton:GetName()
-	sml_dprint(db.Debug,"BestSeller",az)
-  end
-  if(narg[0] == "debug" ) then
-	bsToggle_Debug()
-  end
-  if(narg[0] == "debugallevents" ) then
-	bsToggle_DebugShowAllEvents()
-  end
-  if(narg[0] == "rs" ) then
-	bsRestack()
-	return
-  end
-
-  if(narg[0] == "abl") then
-	sml_print("BestSeller","Auto buy list:")
-	sml_print("BestSeller","=====================================================")
-	bsABList()
-  end
-  if(narg[0] == "ict") then
-	bsCompareItem(GetInventoryItemLink("player", "HeadSlot"))
-  end
-  if(narg[0] == "hardexlist") then
-	bsTable("BS_DO_NOT_SELL")
-  end
-
-  if(narg[0] == "aslist") or
-	(narg[0] == "asl") then
-	bsASLPopulate()
-	BSASLFrame:Show()
-  end
-  if(narg[0] == "asexlist") then
-	asi=1
-	sml_print("BestSeller","Auto sell excluded items:")
-	for asname,astable in pairs(db.AutoSell["Exclude"]) do
-	  sml_print("["..asi.."] "..asname)
-	  asi=asi+1
-	end
-  end
-  if(narg[0] == "asremove") then
-	sml_print("BestSeller","Removing item "..narg[1].." from sell list")
-	asi=1
-	for asname,astable in pairs(db.AutoSell["Items"]) do
-	  if(tonumber(asi)==tonumber(narg[1])) then
-		sml_print("["..asi.."] "..asname.." removed")
-		db.AutoSell["Items"][asname]=nil
-		return
-	  end
-	  asi=asi+1
-	end
-  end
-  if(narg[0] == "asexremove") then
-	sml_print("BestSeller","Removing item "..narg[1].." from exclude list")
-	asi=1
-	for asname,astable in pairs(db.AutoSell["Exclude"]) do
-	  if(tonumber(asi)==tonumber(narg[1])) then
-		sml_print("["..asi.."] "..asname.." removed")
-		db.AutoSell["Exclude"][asname]=nil
-		return
-	  end
-	  asi=asi+1
-	end
-  end
-  if(narg[0] == "asadd") then
-  end
-  if(narg[0] == "asexadd") then
-	for what in string.gmatch(msg, "asexadd (.+)") do
-	  if(what~=nil) then
-		sml_dprint(db.Debug,"BestSeller","what=["..what.."]")
-	  end
-	end
-	db.AutoSell["Exclude"][narg[1]]=true
-  end
-  if(narg[0] == "aspop") then
-	bsASLPopulate()
-  end
+  or(    msg == "?" ) then bsShowHelp() return end
+  if(narg[0] == "hoverframeinfo")
+  or(    msg == "hfi") then local curframe=GetMouseFocus():GetName() sml_print("BestSeller",curframe) end
+  if(narg[0] == "who") then SendWho('80') SortWho('Guild') numWhos, totalCount = GetNumWhoResults() sml_dprint(db.Debug,"BestSeller",numWhos.." "..totalCount) end
+  if(narg[0] == "ssz") then local az=MinimapZoneTextButton:GetName() sml_dprint(db.Debug,"BestSeller",az) end
+  if(narg[0] == "debug" ) then bsToggle_Debug() end if(narg[0] == "debugallevents" ) then bsToggle_DebugShowAllEvents() end
+  if(narg[0] == "rs" ) then bsRestack() return end
+  if(narg[0] == "abl") then sml_print("BestSeller","Auto buy list:") sml_print("BestSeller","=====================================================") bsABList() end
+  if(narg[0] == "ict") then bsCompareItem(GetInventoryItemLink("player", "HeadSlot")) end
+  if(narg[0] == "hardexlist") then bsTable("BS_DO_NOT_SELL") end
+  if(narg[0] == "aslist")
+  or(narg[0] == "asl") then bsASLPopulate() BSASLFrame:Show() end
+  if(narg[0] == "asexlist") then asi=1 sml_print("BestSeller","Auto sell excluded items:") for asname,astable in pairs(db.AutoSell["Exclude"]) do sml_print("["..asi.."] "..asname) asi=asi+1 end end
+  if(narg[0] == "asremove") then sml_print("BestSeller","Removing item "..narg[1].." from sell list") asi=1 for asname,astable in pairs(db.AutoSell["Items"]) do if(tonumber(asi)==tonumber(narg[1])) then sml_print("["..asi.."] "..asname.." removed") db.AutoSell["Items"][asname]=nil return end asi=asi+1 end end
+  if(narg[0] == "asexremove") then sml_print("BestSeller","Removing item "..narg[1].." from exclude list") asi=1 for asname,astable in pairs(db.AutoSell["Exclude"]) do if(tonumber(asi)==tonumber(narg[1])) then sml_print("["..asi.."] "..asname.." removed") db.AutoSell["Exclude"][asname]=nil return end asi=asi+1 end end
+  if(narg[0] == "asadd") then end
+  if(narg[0] == "asexadd") then for what in string.gmatch(msg, "asexadd (.+)") do if(what~=nil) then sml_dprint(db.Debug,"BestSeller","what=["..what.."]") end end db.AutoSell["Exclude"][narg[1]]=true end
+  if(narg[0] == "aspop") then bsASLPopulate() end
 end
--- BestSellerEvents.lua
+++ BestSellerEvents.lua
@@ -1,46 +1,30 @@
 -- Best Seller Event Handler
 bsRecentAutoSold={}
-function bsRegisterEvent(event)
-  if( event ~= nil ) then
-	BSFrame:RegisterEvent(event)
-  end
-end
 lastitembuyback=0
-function bsOnEvent(self, event, ...)
+function bsRegisterEvent(event) if(event ~= nil)then BSFrame:RegisterEvent(event) end end
+function bsOnEvent(self,event,...)
   bsSetDebugWindow()
-  local arg1, arg2 = ...
-  local nnames, nnum
+  local arg1,arg2 = ...
+  local nnames,nnum
   narg=sml_nargify(arg1)
-  if (db) then
-    if (db.DebugAllEvents) then
-      sml_dprint(db.Debug,"BestSeller"," BEST SELLER EVENT ["..event.."]")
-    end
-  end
-  if (event == "VARIABLES_LOADED") then
-	if( not BestSellerDatabase ) then
-	  BestSellerDatabase={}
-	end
-	if( not BestSellerDatabase[GetRealmName()] ) then
-	  BestSellerDatabase[GetRealmName()]={}
-	end
-	if( not BestSellerDatabase[GetRealmName()][UnitName("player")] ) then
-	  BestSellerDatabase[GetRealmName()][UnitName("player")]={}
-	end
- 	db = BestSellerDatabase[GetRealmName()][UnitName("player")]
-	if(db) then  --- set up default values
-	  if(db.BestSellerDebug==nil) then
-		db.BestSellerDebug = 0
-	  end
+  if(db)then if(db.DebugAllEvents)then sml_dprint(db.Debug,"BestSeller"," BEST SELLER EVENT ["..event.."]") end end
+  if(event=="VARIABLES_LOADED")then
+	if(not BestSellerDatabase)then BestSellerDatabase={} end
+	if(not BestSellerDatabase[GetRealmName()])then BestSellerDatabase[GetRealmName()]={} end
+	if(not BestSellerDatabase[GetRealmName()][UnitName("player")])then BestSellerDatabase[GetRealmName()][UnitName("player")]={} end
+ 	db   = BestSellerDatabase[GetRealmName()][UnitName("player")]
+	if(db)then
+	  if(db.BestSellerDebug==nil)then db.BestSellerDebug = 0 end
       db["Version"]	  = BestSeller_Version
       db["Name"]	  = UnitName("player")
       db["Server"]	  = GetRealmName()
       db["Locale"]	  = GetLocale()
       db["Race"],
-      db["RaceEn"]    =UnitRace("player")
+      db["RaceEn"]    = UnitRace("player")
       db["Class"],
-      db["ClassEn"]   =UnitClass("player")
+      db["ClassEn"]   = UnitClass("player")
       db["FactionEn"],
-      db["Faction"]   =UnitFactionGroup("player")
+      db["Faction"]   = UnitFactionGroup("player")
       if(db.Enabled==nil)                 then db.Enabled = 1 end
       if(db.AutoRepair==nil)              then db.AutoRepair = 0 end
       if(db.GuildFundedRepair==nil)       then db.GuildFundedRepair = 0 end
@@ -69,18 +53,18 @@ function bsOnEvent(self, event, ...)
     BSAMFramePanel1CheckAddMem:SetChecked(db.AutoSell["AddASL"])
     BSAMFramePanel1CheckDetail:SetChecked(db.AutoSell["Detail"])
     BSAMFramePanel1CheckLowFoods:SetChecked(db.AutoSell["LowFoods"])
-	BSMMButton:SetPoint("CENTER", "UIParent", "BOTTOMLEFT", (db.MMX or 512), (db.MMY or 384))
+	BSMMButton:SetPoint("CENTER","UIParent","BOTTOMLEFT",(db.MMX or 512),(db.MMY or 384))
 	BSAMFramePanel1CheckHideMinimapIcon:SetChecked(db.HideMinimapIcon["Hide"])
-	if(db.HideMinimapIcon["Hide"]==1) then BSMMButton:Hide() end
+	if(db.HideMinimapIcon["Hide"]==1)then BSMMButton:Hide() end
     BSAMFramePanel1CheckSoulbound:SetChecked(db.AutoSell["Soulbound"])
-    if(db.AutoSell["SoulboundQuality"]==nil) then db.AutoSell["SoulboundQuality"]=0 end
-    if(db.AutoSell["SoulboundQuality"]~=nil) then
+    if(db.AutoSell["SoulboundQuality"]==nil)then db.AutoSell["SoulboundQuality"]=0 end
+    if(db.AutoSell["SoulboundQuality"]~=nil)then
       UIDropDownMenu_SetSelectedValue(BSAMFramePanel1ComboBox1,db.AutoSell["SoulboundQuality"])
       BSAMFramePanel1ComboBox1Text:SetText(smldb.ITEM_QUALITY[db.AutoSell["SoulboundQuality"]])
     end
-    if(db.AutoSell["SoulboundLow"]==nil) then db.AutoSell["SoulboundLow"]=2 end
+    if(db.AutoSell["SoulboundLow"]==nil)then db.AutoSell["SoulboundLow"]=2 end
     BSAMFramePanel1SoulboundLow:SetText(db.AutoSell["SoulboundLow"])
-    if(db.AutoSell["SoulboundHigh"]==nil) then db.AutoSell["SoulboundHigh"]=74 end
+    if(db.AutoSell["SoulboundHigh"]==nil)then db.AutoSell["SoulboundHigh"]=74 end
     BSAMFramePanel1SoulboundHigh:SetText(db.AutoSell["SoulboundHigh"])
     BSAMFramePanel1CheckSellBOPAutoDungeon:SetChecked(db.AutoSell["BOPDungeon"])
     BSAMFramePanel1CheckQuestHigh:SetChecked(db.QuestHigh)
@@ -90,7 +74,7 @@ function bsOnEvent(self, event, ...)
     BSAMFramePanel3CheckAutoRepair:SetChecked(db.AutoRepair)
     BSAMFramePanel3CheckGuildFundedRepair:SetChecked(db.GuildFundedRepair)
     BSAMFramePanel3CheckReputationCheck:SetChecked(db.ReputationRepair)
-    if(db.RepairReputation~=nil) then
+    if(db.RepairReputation~=nil)then
       UIDropDownMenu_SetSelectedValue(BSAMFramePanel3ComboReputation,db.RepairReputation)
       BSAMFramePanel3ComboReputationText:SetText(smldb.FACTION_STANDING[db.RepairReputation])
     end
@@ -100,56 +84,44 @@ function bsOnEvent(self, event, ...)
     bsABLCreateButtons()
   end
   --[UPDATE_CHAT_WINDOWS]---------------------------------------------------
-  if ( event == "UPDATE_CHAT_WINDOWS" ) then
+  if(event=="UPDATE_CHAT_WINDOWS")then
 
   end
   --[PLAYER_LOGOUT]---------------------------------------------------------
-  if ( event == "PLAYER_LOGOUT" ) then
+  if(event=="PLAYER_LOGOUT")then
     db.AutoSell["BOE"]=nil
     db.AutoSell["BOEQuality"]=nil
     db.AutoSell["BOELow"]=nil
     db.AutoSell["BOEHigh"]=nil
     db.bsChatFrame = nil
     db.WTF=nil
-    db.MMX, db.MMY = BSMMButton:GetCenter()
+    db.MMX,db.MMY = BSMMButton:GetCenter()
   end
   --[UPDATE_CHAT_WINDOWS]---------------------------------------------------
-  if ( event == "CHAT_MSG_LOOT" ) then
+  if(event=="CHAT_MSG_LOOT")then
     local nnames
     for dditem,ddnum in string.gmatch(arg1,"You receive loot: (\[.+\])(%d)") do
-      if ( dditem == nil ) then dditem="(error)" end
-      if ( ddnum == nil ) then ddnum=1 end
+      if(dditem==nil)then dditem="(error)" end
+      if(ddnum==nil)then ddnum=1 end
       sml_dprint(db.Debug,"BestSeller",arg1.." TURNS INTO:")
       sml_dprint(db.Debug,"BestSeller",dditem..","..ddnum)
       nnum=ddnum
       nnames=GetItemInfo(dditem)
     end
-    if ( nnames ~= nil ) then
-      ncount=GetItemCount( nnames )+nnum
-      if(ncount>0) then
+    if(nnames ~= nil)then
+      ncount=GetItemCount(nnames)+nnum
+      if(ncount>0)then
 		sml_print("BestSeller","COUNTING "..nnames..":"..ncount)
       end
     end
   end
-  if ( event == "UNIT_INVENTORY_CHANGED" ) then
+  if(event=="UNIT_INVENTORY_CHANGED")then
   end
-  if ( event == "QUEST_DETAIL" ) then
-    if( db.QuestAccept == 1 ) then
-      AcceptQuest()
-    end
+  if(event=="QUEST_DETAIL")then if(db.QuestAccept==1)then AcceptQuest() end end
+  if(event=="QUEST_PROGRESS")then	--[[ if(db.QuestComplete==1)then		local complete_button= _G["QuestFrameCompleteQuestButton"]			if(complete_button)then				complete_button:Click()            --CompleteQuest()            end        end	]]--
   end
-  if ( event == "QUEST_PROGRESS" ) then
-	--[[ if(db.QuestComplete==1) then
-		local complete_button= _G["QuestFrameCompleteQuestButton"]
-			if(complete_button) then
-				complete_button:Click()
-            --CompleteQuest()
-            end
-        end
-	]]--
-  end
-  if (event=="QUEST_COMPLETE") then
-	local high, high_index = 0, 0
+  if(event=="QUEST_COMPLETE")then
+	local high,high_index = 0,0
 	local getlink=nil
 	local equippedhigher=0
 	local ilvl=0
@@ -158,37 +130,37 @@ function bsOnEvent(self, event, ...)
 	local equippedlink=nil
 	local equippedilvl=0
 	local thelink=nil
-    if(db.QuestHigh==1) then
+    if(db.QuestHigh==1)then
       sml_print("BestSeller","Number of quest rewards:"..GetNumQuestChoices())
 	  for x=1,GetNumQuestChoices() do
         thelink=GetQuestItemLink("choice",x)
-        if(thelink) then
-          price = select(11, GetItemInfo(thelink))
-		  ilvl  = select(4, GetItemInfo(thelink))
-		  islot = select(9, GetItemInfo(thelink))
-		  equippedlink = GetInventoryItemLink("player", islot)
+        if(thelink)then
+          price = select(11,GetItemInfo(thelink))
+		  ilvl  = select(4,GetItemInfo(thelink))
+		  islot = select(9,GetItemInfo(thelink))
+		  equippedlink = GetInventoryItemLink("player",islot)
 		  equippedilvl = 0
-		  if(equippedlink) then
-			equippedilvl = select( 4, GetItemInfo(equippedlink))
+		  if(equippedlink)then
+			equippedilvl = select(4,GetItemInfo(equippedlink))
 		  end
-		  if(ilvl > equippedilvl) then
+		  if(ilvl > equippedilvl)then
 			equippedhigher=1
 		  end
           if price > high then
-			high, high_index = price, x
+			high,high_index = price,x
 			getlink = thelink
           end
         end
 	  end
 	  local button = _G["QuestInfoItem"..high_index]
-      if button then
+      if(button)then
 		sml_print("BestSeller","Auto selected "..getlink.." as highest vendor value quest reward.")
 		button:Click()
 	  end
-	  if(db.QuestComplete==1) then
-		if(equippedhigher==0) then
+	  if(db.QuestComplete==1)then
+		if(equippedhigher==0)then
 		  local complete_button= _G["QuestFrameCompleteQuestButton"]
-		  if(complete_button) then
+		  if(complete_button)then
 			complete_button:Click()
 		  end
 		  sml_print("BestSeller","Completed quest automatically based on your settings.")
@@ -198,36 +170,36 @@ function bsOnEvent(self, event, ...)
 	  end
 	end
   end
-  if (event == "MERCHANT_SHOW") then
+  if(event=="MERCHANT_SHOW")then
 	local whatfact=bsTargetFaction()
-	if(whatfact==nil) then whatfact=0; end
-	if(db.ReputationRepair==nil) then db.ReputationRepair=0; end
+	if(whatfact==nil)then whatfact=0; end
+	if(db.ReputationRepair==nil)then db.ReputationRepair=0; end
 	local dorepair=1
 	bMerchantOpen=1
 	bAutoSellActive=1
 	bsAutoSell()
 	bAutoSellActive=0
-	if(CanMerchantRepair()) then
-	  if(db.AutoRepair==1) then
-		if(db.ReputationRepair==1) then
-		  if(db.RepairReputation>whatfact) then
+	if(CanMerchantRepair())then
+	  if(db.AutoRepair==1)then
+		if(db.ReputationRepair==1)then
+		  if(db.RepairReputation>whatfact)then
 			dorepair=0
 			sml_print("BestSeller",YFCC.."Your reputation with the merchant's faction: "..smldb.FACTION_STANDING[whatfact]..RFCC.." (AUTOREPAIR NO)")
 		  else
 			sml_print("BestSeller",YFCC.."Your reputation with the merchant's faction: "..smldb.FACTION_STANDING[whatfact]..GFCC.." (AUTOREPAIR YES)")
 		  end
 		end
-		if(dorepair==1) then
-		  local repairAllCost, canRepair = GetRepairAllCost()
-		  if(repairAllCost==nil) then
+		if(dorepair==1)then
+		  local repairAllCost,canRepair = GetRepairAllCost()
+		  if(repairAllCost==nil)then
 			repairAllCost=0
 		  end
-		  if(repairAllCost>0) then
-			if(db.GuildFundedRepair~=1) then
+		  if(repairAllCost>0)then
+			if(db.GuildFundedRepair~=1)then
 			  db.GuildFundedRepair=0
 			end
 			RepairAllItems(db.GuildFundedRepair)
-			if(db.GuildFundedRepair==1) then
+			if(db.GuildFundedRepair==1)then
 			  sml_print("BestSeller","All items repaired using guild funds. Total cost "..GetCoinText(repairAllCost))
 			else
 			  sml_print("BestSeller","All items repaired using personal funds. Total cost "..GetCoinText(repairAllCost))
@@ -238,39 +210,39 @@ function bsOnEvent(self, event, ...)
 	end
 	bsAutoBuy()
   end
-  if (event == "MERCHANT_CLOSED") then
+  if(event=="MERCHANT_CLOSED")then
 	bMerchantOpen=0
 	bsRecentAutoSold=nil
 	bsRecentAutoSold={}
   end
-  if (event == "MERCHANT_UPDATE") then
+  if(event=="MERCHANT_UPDATE")then
 	local numitms=GetNumBuybackItems()
 	sml_dprint(db.Debug,"BestSeller","Num Buyback Items:"..numitms)
 	local sitem=GetBuybackItemLink(GetNumBuybackItems())
-	if(bAutoSellActive==0) then
+	if(bAutoSellActive==0)then
 	  local curframe
 	  if(GetMouseFocus():GetName()~=nil)
 		then curframe=GetMouseFocus():GetName()
-		if(curframe~=nil) then
+		if(curframe~=nil)then
 		  sml_dprint(db.Debug,"BestSeller","curframe:"..curframe)
 		end
 	  end
 	  if(curframe==nil)
 		then curframe=""
 	  end
-	  if(string.find(curframe,"Merchant")) then
-		if (iLastItemSold~=nil) then
-		  sml_dprint(db.Debug,"BestSeller",">>> NOT SELLING, BUYING BACK >>> "..GetItemInfo(iLastItemSold))
+	  if(string.find(curframe,"Merchant"))then
+		if(iLastItemSold~=nil)then
+		  sml_dprint(db.Debug,"BestSeller",">>> NOT SELLING,BUYING BACK >>> "..GetItemInfo(iLastItemSold))
 		end
-		if(iLastItemSold~=nil) then
+		if(iLastItemSold~=nil)then
 		  bsRemoveSellItem(GetItemInfo(iLastItemSold))
 		end
 	  else
-		if(sitem~=nil) then
-		  if(bsWasAutoSold(GetItemInfo(sitem))==1) then
-			sml_dprint(db.Debug,"BestSeller",sitem.." was auto sold, not adding to list")
+		if(sitem~=nil)then
+		  if(bsWasAutoSold(GetItemInfo(sitem))==1)then
+			sml_dprint(db.Debug,"BestSeller",sitem.." was auto sold,not adding to list")
 		  else
-			if(db.AutoSell["AddASL"]==1) then
+			if(db.AutoSell["AddASL"]==1)then
 			  bsAddSellItem(sitem)
 			end
 		  end
